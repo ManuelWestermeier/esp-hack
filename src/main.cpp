dd2958790094c2af7c68cb6e14ec9000bd76d150
ManuelWestermeier <westermeier111@gmail.com>
2026-05-05 13:59:18 +0200

    ack.

--- src/main.cpp
+++ src/main.cpp
@@ -14,10 +14,7 @@ RF24 radio(CE, CSN);
 const byte addr[6] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
 
 uint8_t bleChannels[] = {37, 38, 39};
-uint8_t blueToothChannels[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
-                               10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
-                               20, 21, 22, 23, 24, 25, 26, 27, 28, 29,
-                               30, 31, 32, 33, 34, 35, 36, 37, 38, 39};
+uint8_t blueToothChannels[] = {37, 38, 39};
 
 // 32 Byte Payload (max)
 uint8_t payload[32] = "HELLO__________________________";
@@ -50,8 +47,14 @@ void setup()
   Serial.println("RF24 broadcast ready");
 }
 
+int i = 0;
 void loop()
 {
+  i++;
+  if (i % 100000 == 0)
+  {
+    Serial.println("Broadcasting...");
+  }
   uint8_t ch = blueToothChannels[random(0, sizeof(blueToothChannels))];
   radio.setChannel(ch);
   radio.write(&payload, sizeof(payload));
